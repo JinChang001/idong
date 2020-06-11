041d7deaa48ee46d1f2962d6000f63f4db90c2b2
Liu Qinqiu <aoxs_98@163.com>
2020-06-11 17:54:49 +0800

    增加moment组件

--- src/main.js
+++ src/main.js
@@ -8,12 +8,17 @@ import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.min.css'
 import MyTabbar from './components/MyTabbar'
 import MyHeader from './components/MyHeader'
+//通过Vue.use()方法使用MintUI插件
+Vue.use(MintUI);
+
 //导入Vant
 import VantUI from "vant";
 import "vant/lib/index.css";
 Vue.use(VantUI);
-//通过Vue.use()方法使用MintUI插件
-Vue.use(MintUI);
+
+//导入moment
+import moment from 'moment';
+Vue.prototype.moment = moment;
 
 axios.defaults.baseURL = 'http://127.0.0.1:9001'
 Vue.prototype.axios = axios;
